f383686bf48dde949bd4e18fb8cd5aba02d65c21
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-02-22 08:39:55 +0200

    add layout components for create login later

---
+++ resources/js/components/LayoutFull.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="wrapper">
+        <!--TODO header/footer/sidebar/content (maybe content+title+breadcrumbs)-->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LayoutFull"
+}
+</script>
---
+++ resources/js/components/LayoutMini.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-auto min-vh-100">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LayoutMini"
+}
+</script>
