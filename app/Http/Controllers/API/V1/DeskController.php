3d3e674d10b6a9fc68c63bb1bddd80425796e713
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-02-24 08:46:47 +0200

    add debugbar

--- app/Http/Controllers/API/V1/DeskController.php
+++ app/Http/Controllers/API/V1/DeskController.php
@@ -13,7 +13,7 @@ final class DeskController extends Controller
 {
     public function index()
     {
-        $desks = (new Desks())->getAllDesk();
+        $desks = (new Desks())->getAllDesk(true);
         return JsonResource::collection($desks);
     }
 
