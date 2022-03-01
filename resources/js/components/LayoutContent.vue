29ba57d44dbc5c0ff1543720bfaabf72de3772e4
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-03-01 08:36:54 +0200

    add layout content component

---
+++ resources/js/components/LayoutContent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="content-wrapper">
+        <section class="content-header">
+            <!--TODO title-->
+            <!--TODO breadcrumbs-->
+        </section>
+        <section class="content">
+            <!--TODO content-->
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    //TODO subcomponents or move full markup to parent component
+}
+</script>
